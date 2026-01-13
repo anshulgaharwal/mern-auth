2ee50848e85521290f814487abe70705673c07af
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-13 19:17:22 +0530

    changed util path

--- auth-frontend/src/components/auth/EmailForm.jsx
+++ auth-frontend/src/components/auth/EmailForm.jsx
@@ -1,4 +1,12 @@
+import { startAuth } from "../../utils/api";
+
 const EmailForm = ({ setStep }) => {
+  const handleContinue = async () => {
+    try{
+      const email = document.querySelector("#email").value;
+      const data = await startAuth
+    }
+  }
   return (
     <>
       <h2>Continue with email</h2>
@@ -6,13 +14,7 @@ const EmailForm = ({ setStep }) => {
       <input type="email" placeholder="Email address" />
       <input type="password" placeholder="Password" />
 
-      <button
-        className="primary"
-        onClick={() => {
-          console.log("Email continue");
-          setStep("otp");
-        }}
-      >
+      <button className="primary" onClick={handleContinue}>
         Continue
       </button>
     </>
