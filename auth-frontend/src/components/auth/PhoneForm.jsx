271304e05ae981fc8bb809fc52b8a7ec8323110d
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-13 19:31:42 +0530

    added handleSendOtp fnc in PhoneForm

--- auth-frontend/src/components/auth/PhoneForm.jsx
+++ auth-frontend/src/components/auth/PhoneForm.jsx
@@ -1,17 +1,24 @@
+import { startAuth } from "../../utils/api";
+
 const PhoneForm = ({ setStep }) => {
+  const handleSendOtp = async () => {
+    try {
+      const phone = document.querySelector("#phone").value;
+      const data = await startAuth({ phone });
+      if (data.next === "PHONE_OTP") {
+        setStep("otp");
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+  };
   return (
     <>
       <h2>Continue with phone</h2>
 
       <input type="tel" placeholder="Mobile number" />
 
-      <button
-        className="primary"
-        onClick={() => {
-          console.log("Phone OTP");
-          setStep("otp");
-        }}
-      >
+      <button className="primary" onClick={handleSendOtp}>
         Send OTP
       </button>
     </>
